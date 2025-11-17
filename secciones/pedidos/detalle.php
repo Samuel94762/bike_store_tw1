f8791940676a19cc8dfbdd36849e54adecf0e878
SCPC 401 <SCPC401@labs.upds>
2025-11-17 10:28:12 -0400

    avance detalle pedidos

---
+++ secciones/pedidos/detalle.php
@@ -0,0 +1,64 @@
+<?php include("../../bd.php");
+
+// Eliminar pedido
+if(isset($_GET['txtID'])){
+    $txtID = (isset($_GET['txtID'])) ? $_GET['txtID'] : "";
+}
+    // Cambiar estado del pedido
+    $sentencia = $conexion->prepare("SELECT *,(SELECT product_name FROM products WHERE product_id=order_items.product_id LIMIT 1) AS producto FROM order_items WHERE order_id=$txtID ");
+    $sentencia->execute();
+    $lista_pedidos_detalle = $sentencia->fetchAll(PDO::FETCH_ASSOC);
+    function restarPorcentaje($valor, $porcentaje){
+        return number_format($valor - ($valor *($porcentaje* 100)/100), 2, '.', ',');
+    }
+?>
+
+<?php include("../../templates/header.php") ?>
+
+<br>
+<div class="card">
+    <div class="card-header">
+        <a name="" id="" class="btn btn-outline-secondary" href="index.php" role="button"><i class= "bi bi-plus-circle me-2"></i>Atr&aacute;s</a>
+    </div>
+    <div class="card-body">
+        <div class="table-responsive-sm">
+            <table class="table table-primary">
+                <thead>
+                    <tr>
+                        <th scope="col">Item</th>
+                        <th scope="col">Producto</th>
+                        <th scope="col">Cantidad</th>
+                        <th scope="col">Precio</th>
+                        <th scope="col">Descuento</th>
+                        <th scope="col">Sub Total</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach($lista_pedidos_detalle as $pedido) { $item=1; ?>
+                    <tr class="">
+                        <td scope="row"><?php echo $item; ?></td>
+                        <td><?php echo $pedido['producto']; ?></td>
+                        <td><?php echo $pedido['quantity']; ?></small></td>
+                        <td><?php echo $pedido['price']; ?></td>
+                        <td><?php echo $pedido['discount']; ?></td>
+                        <td><?php echo restarPorcentaje($pedido['quantity']* $pedido['price'], 
+                                                        $pedido['discount']); $item++; ?></td>
+                        
+                        
+                    </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
+        </div>
+        
+    </div>
+    <div class="card-footer text-muted">
+        <?php 
+        if(isset($_GET['mensaje'])) {
+            echo $_GET['mensaje'];
+        }
+        ?>
+    </div>
+</div>
+
+<?php include("../../templates/footer.php") ?>
